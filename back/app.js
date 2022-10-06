85e840356f7cd4b3d79d2a4b8f281f669abafff0
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-06 07:45:06 -0500

    [ADD] productController productRoute

--- back/app.js
+++ back/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+//importar rutas
+const products = require('./routes/productRoute');
+
+
+app.use("/api", products);
 
 module.exports = app;
