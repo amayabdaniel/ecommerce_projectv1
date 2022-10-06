402447f9d7b6b846e45b275690ad0969a4f0b64c
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-06 07:05:02 -0500

    [ADD] environments

--- back/server.js
+++ back/server.js
@@ -1,3 +1,10 @@
 const app = require('./app');
 
-const server = app.listen
+
+const dotenv = require('dotenv');
+dotenv.config({ path: 'back/config/config.env' });
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT} in mode ${process.env.NODE_ENV}`);
+});
