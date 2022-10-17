04eb7d79af3f57c0679571aa1626306160d9de41
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-17 18:35:51 -0500

    [ADD] GET PRODUCTS ID

--- back/controllers/productController.js
+++ back/controllers/productController.js
@@ -1,13 +1,32 @@
 const producto = require("../models/productos");
 
 //Ver la lista de productos
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+
+    const productos = await producto.find();
     res.status(200).json({
-        success: true,
-        message: 'This route will show all products in database'
+      success: true,
+      count: productos.length,
+      productos
     });
 };
+//ver un producto por id
+exports.getProductById = async (req, res, next) => {
+    const product = await producto.findById(req.params.id);
 
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Producto no encontrado"
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        mensaje: "Producto encontrado",
+        product
+    });
+};
 //Crear nuevo producto /api/productos
 exports.newProduct = async (req, res, next) => {
     const product = await producto.create(req.body);
--- back/routes/productRoute.js
+++ back/routes/productRoute.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProducts} = require('../controllers/productController');
-const {newProduct} = require('../controllers/productController');
-
+const {getProducts, newProduct, getProductById} = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
 router.route("/products/new").post(newProduct);
+router.route('/products/:id').get(getProductById);
 
 
 
