c04a4bd285e731382fe079edef4f85030a7f6cd5
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-06 18:55:23 -0500

    [ADD] home v2

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -3,20 +3,29 @@ import React, { Fragment } from 'react'
 export const Home = () => {
   return (
     <Fragment>
-        <h1 id='encabezado_productos'>últimos Productos</h1>
+      <h1 id="encabezado_productos">últimos Productos</h1>
 
-        <section id='productos' className='container mt-5'>
-            <div className='row'>
-                <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                    <div className="card p-3 rounded">
-                        <img className='card-img-top' src="../../public/images" alt="" />
-                        <div className="card-body d-flex flex-column">
-                            <h5 id='titulo_producto'><a href="">Titulo del producto</a></h5>
-                        </div>
-                    </div>
+      <section id="productos" className="container mt-5">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+            <div className="card p-3 rounded">
+              <img className="card-img-top" src="../../public/images" alt="" />
+              <div className="card-body d-flex flex-column">
+                <h5 id="titulo_producto">
+                  <a href="">Titulo del producto</a>
+                </h5>
+                <div className="rating mt-auto">
+                  <div className="rating-outer">
+                    <div className="rating-inner"></div>
+                  </div>
+                  <span id="puntaje_producto">5 reviews</span>
                 </div>
+                <p className='card-text'>$72.000</p><a href="#" id='ver_producto' className='btn btn-block'>Ver Detalle del producto</a>
+              </div>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     </Fragment>
-  )
+  );
 }
