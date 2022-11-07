1333735f51216f9a25aefeedd50244923f371ec4
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-06 19:15:04 -0500

    [ADD] home v3

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Header from './components/layouts/Header';
+import Home from './components/Home';
 import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,31 +1,87 @@
 import React, { Fragment } from 'react'
 
 export const Home = () => {
-  return (
-    <Fragment>
-      <h1 id="encabezado_productos">últimos Productos</h1>
+    return (
+        <Fragment>
+            <h1 id="encabezado_productos">Últimos Productos</h1>
 
-      <section id="productos" className="container mt-5">
-        <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
-              <img className="card-img-top" src="../../public/images" alt="" />
-              <div className="card-body d-flex flex-column">
-                <h5 id="titulo_producto">
-                  <a href="">Titulo del producto</a>
-                </h5>
-                <div className="rating mt-auto">
-                  <div className="rating-outer">
-                    <div className="rating-inner"></div>
-                  </div>
-                  <span id="puntaje_producto">5 reviews</span>
+            <section id="productos" className="container mt-5">
+                <div className="row">
+                    {/* Prod 1 */}
+                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                        <div className="card p-3 rounded">
+                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
+                            <div className="card-body d-flex flex-column">
+                                <h5 id="titulo_producto">
+                                    <a href="http://localhost:3000">Titulo del producto</a>
+                                </h5>
+                                <div className="rating mt-auto">
+                                    <div className="rating-outer">
+                                        <div className="rating-inner"></div>
+                                    </div>
+                                    <span id="puntaje_producto">5 reviews</span>
+                                </div>
+                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='ver_producto' className='btn btn-block'>Ver Detalle del producto</a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Prod 2 */}
+                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                        <div className="card p-3 rounded">
+                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
+                            <div className="card-body d-flex flex-column">
+                                <h5 id="titulo_producto">
+                                    <a href="http://localhost:3000">Titulo del producto</a>
+                                </h5>
+                                <div className="rating mt-auto">
+                                    <div className="rating-outer">
+                                        <div className="rating-inner"></div>
+                                    </div>
+                                    <span id="puntaje_producto">5 reviews</span>
+                                </div>
+                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='ver_producto' className='btn btn-block'>Ver Detalle del producto</a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Prod 3 */}
+                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                        <div className="card p-3 rounded">
+                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
+                            <div className="card-body d-flex flex-column">
+                                <h5 id="titulo_producto">
+                                    <a href="http://localhost:3000">Titulo del producto</a>
+                                </h5>
+                                <div className="rating mt-auto">
+                                    <div className="rating-outer">
+                                        <div className="rating-inner"></div>
+                                    </div>
+                                    <span id="puntaje_producto">5 reviews</span>
+                                </div>
+                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='ver_producto' className='btn btn-block'>Ver Detalle del producto</a>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Prod 4 */}
+                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                        <div className="card p-3 rounded">
+                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
+                            <div className="card-body d-flex flex-column">
+                                <h5 id="titulo_producto">
+                                    <a href="http://localhost:3000">Titulo del producto</a>
+                                </h5>
+                                <div className="rating mt-auto">
+                                    <div className="rating-outer">
+                                        <div className="rating-inner"></div>
+                                    </div>
+                                    <span id="puntaje_producto">5 reviews</span>
+                                </div>
+                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='ver_producto' className='btn btn-block'>Ver Detalle del producto</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <p className='card-text'>$72.000</p><a href="#" id='ver_producto' className='btn btn-block'>Ver Detalle del producto</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </Fragment>
-  );
+            </section>
+        </Fragment>
+    );
 }
+export default Home;
