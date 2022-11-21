db2b50a5af6775c85ddb853882fb0a1f573badd4
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-20 19:57:20 -0500

    [ADD] minor changes with redux

--- front/src/Store.js
+++ front/src/Store.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({})
 
 let initialState = {}
 
 const middleware = [thunk]
-const store = create(
+const store = createStore (
   reducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
