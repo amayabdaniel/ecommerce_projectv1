38c4bf51d08e3db439ffc01775b086bd6a2c57b6
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-21 14:39:55 -0500

    [ADD] making home dynamic

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,94 +1,63 @@
 import React, { Fragment, useEffect } from 'react'
 import MetaData from './layouts/MetaData';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../actions/productActions';
+import { Link } from "react-router-dom";
 
 export const Home = () => {
+
+    const { loading, products, error } = useSelector((state) => state.products);
+
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getProducts());
     }, [dispatch]);
     return (
         <Fragment>
-            <MetaData title="Lo mejor para tu mascota"></MetaData>
-            <h1 id="encabezado_productos">Últimos Productos</h1>
+            {loading ? <h1>Cargando...</h1> : (
+                <Fragment>
+                    <MetaData title="Lo mejor para tu mascota"></MetaData>
+                    <h1 id="encabezado_productos">Últimos Productos</h1>
 
-            <section id="productos" className="container mt-5">
-                <div className="row">
-                    {/* Prod 1 */}
-                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                        <div className="card p-3 rounded">
-                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
-                            <div className="card-body d-flex flex-column">
-                                <h5 id="titulo_producto">
-                                    <a href="http://localhost:3000">Titulo del producto</a>
-                                </h5>
-                                <div className="rating mt-auto">
-                                    <div className="rating-outer">
-                                        <div className="rating-inner"></div>
+                    <section id="productos" className="container mt-5">
+                        <div className="row">
+                            {products &&
+                                products.map((product) => (
+                                    <div
+                                        key={product._id}
+                                        className="col-sm-12 col-md-6 col-lg-3 my-3"
+                                    >
+                                        <div className="card p-3 rounded">
+                                            <img
+                                                className="card-img-top mx-auto"
+                                                src={product.imagen[0].url}
+                                                alt={product.imagen[0].public_id}
+                                            />
+                                            <div className="card-body d-flex flex-column">
+                                                <h5 id="titulo_producto">
+                                                    <Link to={`/product/${product._id}`}>{product.nombre}</Link>
+                                                </h5>
+                                                <div className="rating mt-auto">
+                                                    <div className="rating-outer">
+                                                        <div className="rating-inner" style={{ width: `${(product.calificacion / 5) * 100}%` }}></div>
+                                                    </div>
+                                                    <span id="No_de_opiniones">{product.numCalificaciones} Reviews</span>
+                                                </div>
+                                                <p className="card-text">{"$ " + product.precio}</p>
+                                                <Link to={`/product/${product._id}`}
+                                                    id="view_btn"
+                                                    className="btn btn-block"
+                                                >
+                                                    Ver Detalle
+                                                </Link>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <span id="puntaje_producto">5 reviews</span>
-                                </div>
-                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='view_btn' className='btn btn-block'>Ver Detalle del producto</a>
-                            </div>
+                                ))}
                         </div>
-                    </div>
-                    {/* Prod 2 */}
-                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                        <div className="card p-3 rounded">
-                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
-                            <div className="card-body d-flex flex-column">
-                                <h5 id="titulo_producto">
-                                    <a href="http://localhost:3000">Titulo del producto</a>
-                                </h5>
-                                <div className="rating mt-auto">
-                                    <div className="rating-outer">
-                                        <div className="rating-inner"></div>
-                                    </div>
-                                    <span id="puntaje_producto">5 reviews</span>
-                                </div>
-                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='view_btn' className='btn btn-block'>Ver Detalle del producto</a>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Prod 3 */}
-                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                        <div className="card p-3 rounded">
-                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
-                            <div className="card-body d-flex flex-column">
-                                <h5 id="titulo_producto">
-                                    <a href="http://localhost:3000">Titulo del producto</a>
-                                </h5>
-                                <div className="rating mt-auto">
-                                    <div className="rating-outer">
-                                        <div className="rating-inner"></div>
-                                    </div>
-                                    <span id="puntaje_producto">5 reviews</span>
-                                </div>
-                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='view_btn' className='btn btn-block'>Ver Detalle del producto</a>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Prod 4 */}
-                    <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-                        <div className="card p-3 rounded">
-                            <img className="card-img-top mx-auto" src="./images/nutre1.jpg" alt="" />
-                            <div className="card-body d-flex flex-column">
-                                <h5 id="titulo_producto">
-                                    <a href="http://localhost:3000">Titulo del producto</a>
-                                </h5>
-                                <div className="rating mt-auto">
-                                    <div className="rating-outer">
-                                        <div className="rating-inner"></div>
-                                    </div>
-                                    <span id="puntaje_producto">5 reviews</span>
-                                </div>
-                                <p className='card-text'>$72.000</p><a href="http://localhost:3000" id='view_btn' className='btn btn-block'>Ver Detalle del producto</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                    </section>
+                </Fragment>
+            )}
         </Fragment>
     );
 }
