72d5ac5bf01884a73ae373f9ee1fc13ff78a38c6
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-21 06:45:30 -0500

    [ADD] actions constant reducer folders and files created

---
+++ front/src/reducer/productReducer.js
@@ -0,0 +1,29 @@
+import { ALL_PRODUCTS_FAIL, ALL_PRODUCTS_REQUEST, ALL_PRODUCTS_SUCCESS, CLEAR_ERRORS, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS } from "../constants/productConstants";
+
+export const productsReducer = ({ state = { products: [] }, action }) => {
+    switch (action.type) {
+        case ALL_PRODUCTS_REQUEST:
+            return {
+                loading: true,
+                products: []
+            }
+        case ALL_PRODUCTS_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload.productos,
+                productsCount: action.payload.cantidadProductos,
+            }
+        case ALL_PRODUCTS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+        default:
+            return state;
+    }
+}
