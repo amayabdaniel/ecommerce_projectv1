375726ba730008e16266aea568f197f75f6079f3
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-21 14:51:01 -0500

    [fix] back error handling

--- back/controllers/productController.js
+++ back/controllers/productController.js
@@ -5,6 +5,12 @@ const fetch = (url) => import('node-fetch').then(({ default: fetch }) => fetch(u
 exports.getProducts = async (req, res, next) => {
 
     const productos = await producto.find();
+    if(!productos){
+        return res.status(404).json({
+            success: false,
+            error: true
+        })
+    }
     res.status(200).json({
         success: true,
         count: productos.length,
