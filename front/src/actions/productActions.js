5f3468bfad891b288c0d004baaf0c8f2d00a8638
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-21 20:32:51 -0500

    [ADD] more to product details

--- front/src/actions/productActions.js
+++ front/src/actions/productActions.js
@@ -32,7 +32,7 @@ export const getProductsDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-        const { data } = await axios.get(`/api/products/:${id}`);
+        const { data } = await axios.get(`/api/products/${id}`);
 
         dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
--- front/src/components/products/ProductDetails.jsx
+++ front/src/components/products/ProductDetails.jsx
@@ -1,24 +1,48 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import MetaData from '../layouts/MetaData'
+import { useParams } from 'react-router-dom'
+import { getProductsDetails, clearErrors } from '../../actions/productActions'
+import { useAlert } from 'react-alert'
+import { Carousel } from 'react-bootstrap';
 
 function ProductDetails() {
+    const { loading, product, error } = useSelector(state => state.productDetails)
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const alert = useAlert();
+    useEffect(() => {
+        dispatch(getProductsDetails(id));
+        if (error) {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
+    }, [dispatch, alert, error, id]);
+
     return (
         <Fragment>
-            <MetaData title="Agility alimento blando perros"></MetaData>
-            <div className="row d-flex justify-content-around">
-                <div className="col-12 col-lg-5 img-fluid" id="imagen_producto">
-                    <img
-                        src="../../../public/images/agility.jpg"
-                        height={500}
-                        width={500}
-                        alt="agility"
-                    />
-                </div>
-                <div className="col-12 col-lg-5 mt-5">
-                    <h3>Agility alimento blando perros</h3>
-                    <p id="product_id">Product #3253252</p>
-                </div>
-            </div>
+            {loading ? (
+                <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+            ) : (
+                <Fragment>
+                    <MetaData title={product.nombre}></MetaData>
+                    <div className="row d-flex justify-content-around">
+                        <div className="col-12 col-lg-5 img-fluid" id="imagen_producto">
+                            <Carousel pause="hover">
+                                {product.imagen && product.imagen.map(imagen => (
+                                    <Carousel.Item key={imagen.id}>
+                                        <img className="d-block w-100" src={imagen.url} alt={product.nombre} />
+                                    </Carousel.Item>
+                                ))}
+                            </Carousel>
+                        </div>
+                        <div className="col-12 col-lg-5 mt-5">
+                            <h3>Agility alimento blando perros</h3>
+                            <p id="product_id">Product #3253252</p>
+                        </div>
+                    </div>
+                </Fragment>
+            )}
         </Fragment>
     );
 }
