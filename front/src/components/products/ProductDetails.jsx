728691f2971d47137c0f2749c59955d9deeabc93
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-11-21 21:35:46 -0500

    [ADD] more to product details v2

--- front/src/components/products/ProductDetails.jsx
+++ front/src/components/products/ProductDetails.jsx
@@ -25,20 +25,109 @@ function ProductDetails() {
                 <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
             ) : (
                 <Fragment>
-                    <MetaData title={product.nombre}></MetaData>
+                    <MetaData>{product.nombre}</MetaData>
                     <div className="row d-flex justify-content-around">
                         <div className="col-12 col-lg-5 img-fluid" id="imagen_producto">
                             <Carousel pause="hover">
-                                {product.imagen && product.imagen.map(imagen => (
-                                    <Carousel.Item key={imagen.id}>
-                                        <img className="d-block w-100" src={imagen.url} alt={product.nombre} />
-                                    </Carousel.Item>
-                                ))}
+                                {product.imagen &&
+                                    product.imagen.map((imagen) => (
+                                        <Carousel.Item key={imagen.id}>
+                                            <img
+                                                className="d-block w-100"
+                                                src={"../" + imagen.url}
+                                                alt={product.nombre}
+                                            />
+                                        </Carousel.Item>
+                                    ))}
                             </Carousel>
                         </div>
                         <div className="col-12 col-lg-5 mt-5">
-                            <h3>Agility alimento blando perros</h3>
-                            <p id="product_id">Product #3253252</p>
+                            <h3>{product.nombre}</h3>
+                            <p id="product_id">ID del producto {product._id}</p>
+                            <hr />
+
+                            <div className="rating-outer">
+                                <div
+                                    className="rating-inner"
+                                    style={{ width: `${(product.calificacion / 5) * 100}%` }}
+                                ></div>
+                            </div>
+                            <span id="No_de_reviews">
+                                ({product.numCalificaciones} Reviews)
+                            </span>
+                            <hr />
+                            <p id="precio_producto">${product.precio}</p>
+                            <div className="stockCounter d-inline">
+                                <span className="btn btn-danger minus">-</span>
+                                <input
+                                    type="number"
+                                    className="form-control count d-inline"
+                                    readOnly
+                                />
+                                <span className="btn btn-primary plus">+</span>
+                            </div>
+                            <button
+                                type="button"
+                                id="carrito_btn"
+                                className="btn btn-primary d-inline ml-4"
+                                disabled={product.inventario === 0}
+                            >
+                                Agregar al Carrito
+                            </button>
+                            <hr />
+                            <p>
+                                Estado:
+                                <span
+                                    id="stock_estado"
+                                    className={product.inventario ? "greenColor" : "redColor"}
+                                >
+                                    {product.inventario > 0 ? "Disponible" : "No disponible"}
+                                </span>
+                            </p>
+                            <hr />
+                            <h4 className="mt-2">Descripcion:</h4>
+                            <p>{product.descripcion}</p>
+                            <hr />
+                            <p id="vendedor">
+                                {" "}
+                                Vendido por: <strong>{product.vendedor}</strong>
+                            </p>
+                            <button
+                                id="review_btn"
+                                type="button"
+                                className="btn btn-primary mt-4"
+                                data-toggle="modal"
+                                data-target="#ratingModal"
+                            ></button>
+                                <div className='alert alert-danger mt-5' type='alert'>Inicia Sesión para dekar tu review</div>
+                            {/* <!-- Modal --> */}
+                            <div className='row mt-2 mb-5'>
+                                <div className='rating w-50'>
+                                    <div className='modal fade' id='ratingModal' tabIndex='-1' role='dialog' aria-labelledby='ratingModalLabel' aria-hidden='true'>
+                                        <div className='modal-dialog' role='document'>
+                                            <div className='modal-content'>
+                                                <div className='modal-header'>
+                                                    <h5 className='modal-title' id='ratingModalLabel'>Calificar Producto</h5>
+                                                    <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
+                                                        <span aria-hidden='true'>&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div className='modal-body'>
+                                                    <ul className='stars'>
+                                                        <li className='star'><i className='fa fa-star'></i></li>
+                                                        <li className='star'><i className='fa fa-star'></i></li>
+                                                        <li className='star'><i className='fa fa-star'></i></li>
+                                                        <li className='star'><i className='fa fa-star'></i></li>
+                                                        <li className='star'><i className='fa fa-star'></i></li>
+                                                    </ul>
+                                                        <textarea name='review' id='review' className='form-control mt-3'></textarea>
+                                                        <button className='btn my-3 float-right review-btn px-4 text-white' data-dismiss='modal' aria-label='Close' type='button'>Enviar</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </Fragment>
